fd7511e3cc0db2b55d6ef8dcc82cbbfcb1078a21
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2020-02-18 15:31:07 +0200

    Remove sequilize synk

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-sequelize.sync().then(result=>{
-  // console.log(result);
-})
-.catch(err=> console.log(err));
+// sequelize.sync().then(result=>{
+//   // console.log(result);
+// })
+// .catch(err=> console.log(err));
 
 
 module.exports = app;
