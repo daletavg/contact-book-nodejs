b5d7af1c2074b92147ea167d522bf1ce59da5483
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2020-02-18 15:28:09 +0200

    Change posgre url connect

--- models/dbconnect.js
+++ models/dbconnect.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // const sequelize = new Sequelize('mysql://root:@127.0.0.1:8182/nodejs');
-const sequelize = new Sequelize('postgres://fusflxbyzkfaho:6747f55fa3df9f54b1106041df2e83142aed902375f687bf137a2427cb394e5f@ec2-184-72-236-3.compute-1.amazonaws.com:5432/d8d9bh9r9fsf78');
+const sequelize = new Sequelize('postgres://giupbnvncztwme:bcc4263343a185fcc1d6caf023ec31cde7788e29fe03527eb2610bb4c8f2f846@ec2-184-72-236-3.compute-1.amazonaws.com:5432/dfs50o85gs8hoo');
 
 
 module.exports = sequelize;
