a6bc3947644b964a90009fa9e8b289f8739d3828
Vadim Dubrovskiy <programmer@Mac-mini-Programmer.local>
2020-02-19 14:20:33 +0200

    Added model user, and metod createUserIfNot exist

--- config/telegram.js
+++ config/telegram.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const {User,createUserIfNotExist}=require('../models/users');
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 const bot = new TelegramBot(TOKEN);
@@ -10,7 +11,14 @@ const port = process.env.PORT;
 
 bot.on('message', msg => {
     const user = msg.from;
-
+    if(msg.text === '/reg'){
+        const data = createUserIfNotExist(user);
+        if(data !== null){
+            bot.sendMessage(msg.chat.id, user.first_name+' '+user.last_name+' Registrate successful');
+        }else{
+            bot.sendMessage(msg.chat.id, 'User already exist!');
+        }
+    }
     bot.sendMessage(msg.chat.id, user.first_name+' '+user.last_name);
 });
 
---
+++ models/users.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define("user", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    uiid:{
+        type:Sequelize.BIGINT,
+        allowNull:false,
+    },
+    is_bot:{
+        type:Sequelize.BOOLEAN,
+        allowNull:true,
+    },
+    first_name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    last_name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+    },
+    language_code:{
+        type:Sequelize.STRING,
+        allowNull:true
+    }
+
+});
+const createUserIfNotExist = async (data)=>{
+    let createdUser = null;
+    if(User.findOne({where: {uiid:data.id}})===null) {
+            createdUser = await User.create({
+            uiid: data.id,
+            is_bot: data.is_bot,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            username: data.username,
+            language_code: data.language_code
+        });
+    }
+    return  createdUser;
+};
+
+
+module.exports = {User,createUserIfNotExist};
